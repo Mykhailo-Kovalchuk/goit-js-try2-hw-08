41610e5d1bcc415a587d256da013a1fd0e25b845
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-10-23 08:18:29 +0200

    goit-js-try2-hw-08-commit 4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,41 +9,37 @@ const player = new Player(iframe);
 console.log("qwerty");
 
 
-const savedTimeData = localStorage.getItem("videoplayer-current-time");
-const parsedTimeData = savedTimeData ? JSON.parse(savedTimeData) : null;    
+  const  savedTimeData = localStorage.getItem("videoplayer-current-time");
+  const  parsedTimeData = savedTimeData ? JSON.parse(savedTimeData) : null;   
 
 
-player.on('play', () => {
-    console.log('Played the video!');
 
-    
-    if (parsedTimeData !== null) {
-    
-        player.setCurrentTime(parsedTimeData).then(() => {
-            console.log("Continue playing")
-        });
+// player.on('play', () => {
+//     console.log('Played the video!');
+// });
+
+
+  
+
+if (parsedTimeData !== null) {
+    player.setCurrentTime(parsedTimeData).then(() => {
+        console.log("Continue playing")
+    });
+} else {
+player.setCurrentTime(0).then(() => {
+    console.log("Continue playing")
+});
+
 }
 
 
 
-    player.on('timeupdate', throttle((data) => {
+
+player.on('timeupdate', throttle((data) => {
     const currentTime = data.seconds;
     
      localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
     }, 1000));
-});
-
-
-
-
-
-
-
-
-   
-
-
-
 
 
 
@@ -52,4 +48,4 @@ player.on('play', () => {
 // });
 
 
-console.log("123")
+// console.log("123")
